384c9bc3551bc298bb49ada37f0ed995f3c79222
jeancmr <jean.madiedo08@gmail.com>
2026-06-29 15:33:05 -0500

    feat: add postgresql container and add env.template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,40 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        synchronize: true,
+        autoLoadEntities: true,
+        ssl: configService.get<string>('DB_SSL') === 'true',
+        extra:
+          configService.get<string>('DB_SSL') === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      }),
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  private readonly logger = new Logger(AppModule.name);
+
+  onModuleInit() {
+    this.logger.log('Postgres database was succesfully connected');
+  }
+}
